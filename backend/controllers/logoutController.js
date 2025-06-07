63f9632e7c174424b6264c1313e2c087568f3682
swastik-bhandari <swastikbhandari2006@gmail.com>
2025-06-07 18:13:16 +0545

    feat : handling logout , added : logoutController.js and logoutRoutes.js

--- backend/controllers/logoutController.js
+++ backend/controllers/logoutController.js
@@ -3,3 +3,47 @@ const userDB = {
   setUser : function (data) { this.users = data}
 }
 
+const fsPromises = require('fs').promises;
+const path = require('path');
+
+const handleLogout = async (req ,res)=> {
+
+  const cookies = req.cookies;
+
+  if(!cookies?.jwt) return res.status(401).json({"error" : "no cookies found "});
+
+  const refreshToken = cookies.jwt;
+
+  const foundUser = userDB.users.find(person=>person.refreshToken === refreshToken);
+
+  if(!foundUser) return res.status(203).json({"error" : "no user found in db with that refreshToken"});
+
+  foundUser.refreshToken="";
+
+  /////////// clearing the cookie ///////////////
+  res.clearCookie('jwt' , 
+    {
+      HttpOnly : true,
+      sameSite : 'none',
+      maxAge : 3*24*60*60*1000
+    }
+  );
+
+  //////////// updating the database /////////////
+
+  const otherUsers = userDB.users.filter(person=>person.email !== foundUser.email);
+  userDB.setUser([...otherUsers , foundUser]);
+
+  try {
+    await fsPromises.writeFile(
+      path.join(__dirname , ".." , "models" , "User.json"),
+      JSON.stringify(userDB.users)
+    );
+  } catch(err) {
+    console.log("error in writing in User.json file");
+    res.sendStatus(500);
+  }
+  res.sendStatus(204);
+}
+
+module.exports = handleLogout;

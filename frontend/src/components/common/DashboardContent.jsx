39abdc813b8b071aa8295d590dd0aea8aa4fdc5d
Sijan Bhandari <uhisijan@gmail.com>
2025-06-25 15:06:01 +0545

    Fix: Button Element Delay

--- frontend/src/components/common/DashboardContent.jsx
+++ frontend/src/components/common/DashboardContent.jsx
@@ -4,21 +4,26 @@ import Button from '../ui/Button';
 import LiveDiscussions from './LiveDiscussions';
 
 function DashBoardContent() {
+  const [activeButton, setActiveButton] = useState(0);
+  function handleClick(event) {
+    const targetedEvent = event.target;
+    const index = targetedEvent.dataset.index;
+    setActiveButton(index);
+  }
+
   return (
     <div className="bg-main-elements flex flex-1 flex-col gap-4 p-6">
-      <div className="flex gap-2">
-        <DashBoardNav />
+      <div className="flex gap-2" onClick={event => handleClick(event)}>
+        <DashBoardNav activeButton={activeButton} />
       </div>
-      <div className="">
+      <div>
         <LiveDiscussions />
       </div>
     </div>
   );
 }
-function DashBoardNav() {
-  const [activeButton, setActiveButton] = useState(0);
-  const activeColor = 'bg-[#2F77FF]';
-
+function DashBoardNav({ activeButton }) {
+  const activeColor = 'bg-[#2F77FF] hover:bg-[#255FCC]';
   return (
     <>
       {dashboardContentNavbar.map((item, index) => {
@@ -26,7 +31,7 @@ function DashBoardNav() {
           <Button
             label={item.label}
             key={index}
-            onClick={() => setActiveButton(index)}
+            index={index}
             isActive={index == activeButton}
             activeColor={activeColor}
           />
--- frontend/src/components/ui/Button.jsx
+++ frontend/src/components/ui/Button.jsx
@@ -1,10 +1,12 @@
-function Button({ label, isActive, onClick, activeColor }) {
+function Button({ label, isActive, activeColor, index }) {
   const baseClass =
-    'text-font text-title hover:bg-layout-elements-focus rounded-button-round mr-2 w-[18rem] cursor-pointer px-3 py-2';
-  const finalClass = `${isActive ? activeColor : ''} ${baseClass} `;
+    'text-font text-title rounded-button-round mr-2 w-[18rem] cursor-pointer px-3 py-2';
+  const finalClass = isActive
+    ? `${activeColor} ${baseClass}`
+    : ` ${baseClass} hover:bg-layout-elements-focus`;
 
   return (
-    <button className={`${finalClass}`} onClick={onClick}>
+    <button className={`${finalClass}`} data-index={index}>
       {label}
     </button>
   );

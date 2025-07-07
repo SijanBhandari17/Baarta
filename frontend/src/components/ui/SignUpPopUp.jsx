2ed126aaebdc024756ede09d6065e4f71afea799
Loopfelloff <abhiyanregmi@gmail.com>
2025-07-07 15:03:55 +0545

    dynamic form validation done

--- frontend/src/components/ui/SignUpPopUp.jsx
+++ frontend/src/components/ui/SignUpPopUp.jsx
@@ -59,9 +59,76 @@ function formValidation(user_name , user_email , user_password){
   if(passwordField.type === 'password') passwordField.type = 'text'
   else passwordField.type='password'
  }
-
-
+ function onUserEnter(e)
+ {
+  const usernameField = e.currentTarget;
+  const usernameInfo = usernameField.nextSibling.nextSibling;
+  if(usernameField.value.length === 0) {
+    usernameInfo.classList.remove('visible')
+    usernameInfo.classList.add('invisible')
+    return;
+  }
+  usernameInfo.classList.remove('invisible')
+  usernameInfo.classList.add('visible')
+  if(usernameField.value.length < 8){
+    usernameInfo.innerText = `*username must be at least 8 letter long`
+    usernameInfo.classList.remove('text-green-600')
+    usernameInfo.classList.add('text-red-600')
+  } 
+  else{
+    usernameInfo.innerText = `*valid username`
+    usernameInfo.classList.remove('text-red-600')
+    usernameInfo.classList.add('text-green-600')
+  }
+ }
+ function onEmailEnter(e)
+ {
+  const emailField = e.currentTarget;
+  const emailInfo = emailField.nextSibling.nextSibling;
+  if(emailField.value.length === 0) {
+    emailInfo.classList.remove('visible')
+    emailInfo.classList.add('invisible')
+    return;
+  }
+  emailInfo.classList.remove('invisible')
+  emailInfo.classList.add('visible')
+  if(!regex.test(emailField.value)){
+    emailInfo.innerText = `*invalid email format used`
+    emailInfo.classList.remove('text-green-600')
+    emailInfo.classList.add('text-red-600')
+  } 
+  else{
+    emailInfo.innerText = `*valid email format`
+    emailInfo.classList.remove('text-red-600')
+    emailInfo.classList.add('text-green-600')
+  }
+ }
+ function onPasswordEnter(e)
+ {
+  const passwordField = e.currentTarget;
+  const passwordInfo = passwordField.nextSibling.nextSibling.nextSibling;
+  const regex = /^(?=.*\d)(?=.*[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]).{8,}$/
+  if(passwordField.value.length === 0) {
+    passwordInfo.classList.remove('visible')
+    passwordInfo.classList.add('invisible')
+    return;
+  }
+  passwordInfo.classList.remove('invisible')
+  passwordInfo.classList.add('visible')
+  if(!regex.test(passwordField.value)){
+    passwordInfo.innerText = `*atleast 1 symbol and number required with 8 letter long`
+    passwordInfo.classList.remove('text-green-600')
+    passwordInfo.classList.add('text-red-600')
+  } 
+  else{
+    passwordInfo.innerText = `*valid password format`
+    passwordInfo.classList.remove('text-red-600')
+    passwordInfo.classList.add('text-green-600')
+  }
+  
+ }
   ///////////////////////////////////////////////////////////////////////////
+  
   return (
     <AnimatePresence>
       {isOpen && (
@@ -73,7 +140,7 @@ function formValidation(user_name , user_email , user_password){
           onClick={onClose}
         >
           <motion.div
-            className="bg-layout-elements  rounded-2xl w-[500px] h-[500px] text-font relative shadow-2xl flex flex-col flex-nowrap "
+            className="bg-layout-elements  rounded-2xl w-[500px] h-[550px] text-font relative shadow-2xl flex flex-col flex-nowrap "
             initial={{ scale: 0.85, opacity: 0 }}
             animate={{ scale: 1, opacity: 1 }}
             exit={{ scale: 0.85, opacity: 0 }}
@@ -88,17 +155,20 @@ function formValidation(user_name , user_email , user_password){
               &times;
             </button>
                </h2>
-            <form className="w-full h-fit px-12 pb-2 flex flex-col flex-nowrap gap-6 relative  border-0  border-pink-600" onSubmit ={handleSubmit}>
+            <form className="w-full h-fit px-12 pb-2 flex flex-col flex-nowrap gap-5 relative  border-0  border-pink-600" onSubmit ={handleSubmit}>
               <div className ="relative">
               <input
                 id='username'
                 type="text"
                 placeholder=" "
                 value = {username}
-                onChange={(e)=> setUsername(e.target.value)}
+                onChange={(e)=>{ setUsername(e.target.value)
+                  onUserEnter(e)
+                }}
                 className="w-full peer rounded-button-round py-4 px-5 text-lg bg-layout-elements-focus focus:bg-layout-elements focus:border-royalpurple-dark focus:border-2 duration-300 text-font placeholder:text-font-light focus:outline-none"
               />
-              <label htmlFor="username" className= "absolute left-5 top-0 text-sm text-gray-200   -translate-y-1/2 peer-placeholder-shown:top-1/4   peer-focus:top-0 peer-focus:text-sm peer-focus:text-royalpurple-dark peer-focus:bg-layout-elements peer-focus:-translate-y-1/2 duration-300 peer-placeholder-shown:text-gray-200 peer-placeholder-shown:text-xl peer-placeholder-shown:translate-y-0">Username</label>
+              <label htmlFor="username" className= "absolute left-5 top-0 text-sm text-gray-200   -translate-y-1/2 peer-placeholder-shown:top-1/4   peer-focus:top-0 peer-focus:text-sm peer-focus:text-royalpurple-dark peer-focus:bg-layout-elements peer-focus:-translate-y-1/2 duration-300 peer-placeholder-shown:text-gray-200 peer-placeholder-shown:text-xl peer-placeholder-shown:-translate-y-1/4">Username</label>
+              <span className='test-sm text-red-600 invisible'>*username properly not written</span>
               </div>
               <div className='relative'>
               <input
@@ -106,10 +176,13 @@ function formValidation(user_name , user_email , user_password){
                 type="email"
                 placeholder=" "
                 value = {email}
-                onChange={(e)=> setEmail(e.target.value)}
+                onChange={(e)=> {setEmail(e.target.value)
+                  onEmailEnter(e)
+                }}
                 className="w-full peer rounded-button-round py-4 px-5 text-lg bg-layout-elements-focus focus:bg-layout-elements focus:border-royalpurple-dark focus:border-2 duration-300 text-font placeholder:text-font-light focus:outline-none"
               />
-              <label htmlFor="email" className= "absolute left-5 top-0 text-sm text-gray-200   -translate-y-1/2 peer-placeholder-shown:top-1/4   peer-focus:top-0 peer-focus:text-sm peer-focus:text-royalpurple-dark peer-focus:bg-layout-elements peer-focus:-translate-y-1/2 duration-300 peer-placeholder-shown:text-gray-200 peer-placeholder-shown:text-xl peer-placeholder-shown:translate-y-0">Email</label>
+              <label htmlFor="email" className= "absolute left-5 top-0 text-sm text-gray-200   -translate-y-1/2 peer-placeholder-shown:top-1/4   peer-focus:top-0 peer-focus:text-sm peer-focus:text-royalpurple-dark peer-focus:bg-layout-elements peer-focus:-translate-y-1/2 duration-300 peer-placeholder-shown:text-gray-200 peer-placeholder-shown:text-xl peer-placeholder-shown:-translate-y-1/4">Email</label>
+              <span className='test-sm text-red-600 invisible'>*username properly not written</span>
               </div>
               <div className='relative'>
               <input
@@ -117,11 +190,14 @@ function formValidation(user_name , user_email , user_password){
                 type="password"
                 placeholder=" "
                 value = {password}
-                onChange={(e)=> setPassword(e.target.value)}
+                onChange={(e)=> {setPassword(e.target.value)
+                  onPasswordEnter(e)
+                }}
                 className="w-full peer rounded-button-round py-4 px-5 text-lg bg-layout-elements-focus focus:bg-layout-elements focus:border-royalpurple-dark focus:border-2 duration-300 text-font placeholder:text-font-light focus:outline-none"
               />
-              <div className='absolute bg-[url(../../assets/icons/eye.png)] bg-pink-400 h-[40px] w-[40px] right-[10px] top-0 translate-y-1/4'onClick={onClickEye}></div>
-              <label htmlFor="password" className= "absolute left-5 top-0 text-sm text-gray-200   -translate-y-1/2 peer-placeholder-shown:top-1/4   peer-focus:top-0 peer-focus:text-sm peer-focus:text-royalpurple-dark peer-focus:bg-layout-elements peer-focus:-translate-y-1/2 duration-300 peer-placeholder-shown:text-gray-200 peer-placeholder-shown:text-xl peer-placeholder-shown:translate-y-0">Password</label>
+              <div className='absolute bg-[url(../../assets/icons/eye.png)] bg-pink-400 h-[40px] w-[40px] right-[10px] top-0 translate-y-1/4' onClick={onClickEye}></div>
+              <label htmlFor="password" className= "absolute left-5 top-0 text-sm text-gray-200   -translate-y-1/2 peer-placeholder-shown:top-1/4   peer-focus:top-0 peer-focus:text-sm peer-focus:text-royalpurple-dark peer-focus:bg-layout-elements peer-focus:-translate-y-1/2 duration-300 peer-placeholder-shown:text-gray-200 peer-placeholder-shown:text-xl peer-placeholder-shown:-translate-y-1/4">Password</label>
+              <span className='test-sm text-red-600 invisible'>*username properly not written</span>
               </div>
               <button
                 type="submit"

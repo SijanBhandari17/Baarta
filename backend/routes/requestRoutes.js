e411ba936de577e324f66f8cf2a4c0f0385637d8
Loopfelloff <abhiyanregmi@gmail.com>
2025-07-24 17:57:53 +0545

    added deletion handler for the user to reject admin or moderator request to join the forum or admin request to become the modeartor

--- backend/routes/requestRoutes.js
+++ backend/routes/requestRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const Router = express.Router()
-const {sendInvite , getInvitation , acceptInvitation , getJoinRequest, acceptJoinRequest} = require('../controllers/requestController')
+const {sendInvite , getInvitation , acceptInvitation , getJoinRequest, acceptJoinRequest , removeForumInvite} = require('../controllers/requestController')
 
-Router.route('/sendInvite').post(sendInvite)
-Router.route('/getInvite').get(getInvitation)
-Router.route('/acceptInvite').post(acceptInvitation)
-Router.route('/getJoinInvite').get(getJoinRequest)
-Router.route('/acceptJoinInvite').post(acceptJoinRequest)
+Router.route('/sendInvite').post(sendInvite) // this router works for three different kind of posts , one for promotion to moderator , another one for join_request, finally for forum_invite
+Router.route('/getInvite').get(getInvitation) // this get the request to join the forum or accept the invitation to become the moderator 
+Router.route('/acceptInvite').post(acceptInvitation) // this is to accept invitation to either become modeartor or to be a member of the forum
+Router.route('/getJoinInvite').get(getJoinRequest) // this is for modeartor and admin to see the applications to join the forum
+Router.route('/acceptJoinInvite').post(acceptJoinRequest) // this is for moderator and admin to accept the application to join the forum
+Router.route('/removeInvite').delete(removeForumInvite) // this is to delete the request for becoming the moderator or joinig the forum by the user not the admin or moderator
 module.exports = Router

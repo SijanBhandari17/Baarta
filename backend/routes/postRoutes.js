1495f3c3ad8268f2d6748c90fcf7708683126e9b
Loopfelloff <abhiyanregmi@gmail.com>
2025-07-25 18:10:24 +0545

    added handlers for liking the post , comment and the reply

--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -5,10 +5,12 @@ const {
   getPost,
   updatePost,
   deletePost,
+  upVotePost
 } = require("../controllers/postController");
 const postPicMiddleware = require("../middleware/cloudinaryMiddleware");
 Router.route("/").post(postPicMiddleware, uploadPost);
 Router.route("/").get(getPost);
 Router.route("/").put(postPicMiddleware, updatePost);
 Router.route("/").delete(deletePost);
+Router.route("/upvote").post(upVotePost)
 module.exports = Router;

c886db17a56682ba8ba77abd603389991a6c40ab
Loopfelloff <abhiyanregmi@gmail.com>
2025-07-27 18:57:54 +0545

    added send all user , send single user , send all forum controllers

---
+++ backend/routes/sendAllRoutes.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const Router = express.Router()
+const {sendAllUser , sendAllForum , sendOneUser} = require('../controllers/sendResource')
+
+Router.route('/userprofile').get(sendAllUser)
+Router.route('/forum').get(sendAllForum)
+Router.route('/singleuserprofile').get(sendOneUser)
+
+
+module.exports = Router

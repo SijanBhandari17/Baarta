af4664e8251e52c13af7b7054b6320cc20d1ffb2
Loopfelloff <abhiyanregmi@gmail.com>
2025-08-02 00:51:04 +0545

    fix : previously it allowed duplicated post being saved

--- backend/controllers/savePostController.js
+++ backend/controllers/savePostController.js
@@ -36,7 +36,15 @@ const savePost = async(req ,res)=>{
             return res.status(404).json({"error" : "the post is either deleted or removed"})
         }
 
+        const findDuplicate = foundUser.save_post.find(item => item.toString() === postId)
+
+        if(findDuplicate) {
+            await session.abortTransaction()
+            return res.status(409).json({"error" : "the post is already saved"})
+        }
+
         foundUser.save_post = [...foundUser.save_post , foundPost._id]
+
 
         const result = await foundUser.save({session})
 
@@ -61,7 +69,53 @@ const savePost = async(req ,res)=>{
         await session.endSession()
     }
 
+
 } 
+
+const getSavedPost = async (req , res)=>{
+    const session = await mongoose.startSession()
+    try
+    {
+
+        await session.startTransaction()
+
+        if(!req.user?.email) return res.status(400).json({"error" : "unauthenticated user sent the request"})
+
+        const {email} =  req.user
+
+        const foundUser = await User.findOne({email}).session(session).exec()
+        if(!foundUser)
+        {
+            await session.abortTransaction()
+            return res.status(404).json({"error" : "user not found"})
+        }
+
+        const savedPostIdArray = foundUser.save_post
+
+        const toSendResult = await Promise.all(savedPostIdArray.map(async(item)=>{
+            const findPost = await Post.findOne({_id:item}).session(session).exec()
+            const authorId = await User.findOne({_id : findPost.author_id}).session(session).exec()
+            const authorProfilePic = await Profile.findOne({userId : authorId}).session(session).exec()
+
+            return {...findPost.toObject() , authorName:authorId.username , authorEmail : authorId.email , authorProfilePicLink : authorProfilePic.profilePicLink || "https://res.cloudinary.com/dlddcx3uw/image/upload/v1752323363/defaultUser_cfqyxq.svg" }
+
+        }))
+
+        await session.commitTransaction()
+
+        res.status(201).json({"message" : "successfully saved posts" , "body" : toSendResult})
+         
+    }
+
+    catch(err)
+    {
+        await session.abortTransaction()
+        res.status(500).json({"error" : `${err.stack}`})
+    }
+    finally{
+        await session.endSession()
+    }
+}
 
 const deleteSavedPost = async (req, res)=>{
 
